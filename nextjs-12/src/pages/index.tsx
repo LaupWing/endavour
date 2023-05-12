50d054734b87b06bf778a49e55a01998956bd727
LaupWing <loc_strict@hotmail.com>
2023-05-12 17:14:01 +0200

    update

--- nextjs-12/src/pages/index.tsx
+++ nextjs-12/src/pages/index.tsx
@@ -31,7 +31,7 @@ const Home:NextPage<PageProps> = ({data}) => {
          </header>
          <section className="py-10">
             <div className="grid grid-cols-2">
-               <div className="transform translate-x-[10%] my-auto">
+               <div className="transform translate-x-[10%] my-auto flex flex-col items-start">
                   <h2 className={`text-7xl ${playfair.className} flex flex-col items-start gap-2`}>
                      <span className="border-t-2 border-primary">
                         {data.artObjects[0].title.split(" ")[0]}
@@ -45,7 +45,7 @@ const Home:NextPage<PageProps> = ({data}) => {
                   </h2>
                   <p className="mt-6">By <span className="text-accent font-bold">{data.artObjects[0].principalOrFirstMaker}</span></p>
                   <p className="text-accent italic">
-                     <a href="" className="flex gap-2 items-center">
+                     <a href={data.artObjects[0].links.web} target="_blank" className="flex gap-2 items-center">
                         Link to rijksmuseum <FiExternalLink className="" size={16} />
                      </a>
                   </p>
