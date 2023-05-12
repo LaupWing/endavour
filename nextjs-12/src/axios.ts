46aa0183e58739d85f4034b37910a1cbb6cee5f3
LaupWing <loc_strict@hotmail.com>
2023-05-12 10:39:39 +0200

    succesfully sending data

--- nextjs-12/src/axios.ts
+++ nextjs-12/src/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-   baseURL: `https://www.rijksmuseum.nl/api/nl/collection?key=${process.env.RIJKS_MUSEUM_APIKEY}`,
-   timeout: 1000,
+   baseURL: `https://www.rijksmuseum.nl/api/nl/collection?key=FZJhvrzH`,
+   timeout: 4000,
 })
 
 export default instance
--- nextjs-12/src/pages/index.tsx
+++ nextjs-12/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import { GetServerSideProps } from "next"
+import axios from "../axios"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -8,7 +9,8 @@ interface PageProps {
 
 }
 
-export default function Home() {
+export default function Home({data}: any) {
+   console.log(data)
    return (
       <main
          className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -125,9 +127,12 @@ export default function Home() {
 }
 
 export const getServerSideProps:GetServerSideProps<PageProps> = async () => {
+   const res = await axios.get("")
+   const data = res.data
+   
    return {
       props: {
-         
+         data      
       }
    }
 }
