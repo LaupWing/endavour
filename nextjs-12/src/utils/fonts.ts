1bdd21acad9161e2439f0ab4c88f7689cf10f170
LaupWing <loc_strict@hotmail.com>
2023-05-12 11:22:59 +0200

    added playfair

--- nextjs-12/src/utils/fonts.ts
+++ nextjs-12/src/utils/fonts.ts
@@ -1,6 +1,11 @@
-import { Poppins } from "next/font/google"
+import { Poppins, Playfair_Display } from "next/font/google"
 
 export const poppins = Poppins({ 
+   weight: "400",
+   subsets: ["latin"] 
+})
+
+export const playfair = Playfair_Display({ 
    weight: "400",
    subsets: ["latin"] 
 })
