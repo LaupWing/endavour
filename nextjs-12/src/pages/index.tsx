5dc1f26330703a195ccf488fce9535d62a821a66
LaupWing <loc_strict@hotmail.com>
2023-05-12 17:51:03 +0200

    update

--- nextjs-12/src/pages/index.tsx
+++ nextjs-12/src/pages/index.tsx
@@ -3,13 +3,13 @@ import axios from "@/utils/axios"
 import { playfair, poppins } from "@/utils/fonts"
 import { BsSearch } from "react-icons/bs"
 import { FiExternalLink } from "react-icons/fi"
+import { Hero } from "@/components/Hero"
 
 interface PageProps {
    data: ApiResponse
 }
 
 const Home:NextPage<PageProps> = ({data}) => {
-   console.log(data)
    return (
       <main
          className={`flex bg-contrast py-6 px-14 min-h-screen fixed inset-0 overflow-y-auto w-screen flex-col ${poppins.className}`}
@@ -30,28 +30,9 @@ const Home:NextPage<PageProps> = ({data}) => {
             </div>
          </header>
          <section className="py-10">
-            <div className="grid grid-cols-2">
-               <div className="transform translate-x-[10%] my-auto flex flex-col items-start">
-                  <h2 className={`text-7xl ${playfair.className} flex flex-col items-start gap-2`}>
-                     <span className="border-t-2 border-primary">
-                        {data.artObjects[0].title.split(" ")[0]}
-                     </span>
-                     <span className="transform translate-x-[5%] border-y-2 border-primary">
-                        {data.artObjects[0].title.split(" ").splice(1,3).join(" ")}
-                     </span>
-                     <span className="border-b-2 border-primary">
-                        {data.artObjects[0].title.split(" ").splice(4).join(" ")}
-                     </span>
-                  </h2>
-                  <p className="mt-6">By <span className="text-accent font-bold">{data.artObjects[0].principalOrFirstMaker}</span></p>
-                  <p className="text-accent italic">
-                     <a href={data.artObjects[0].links.web} target="_blank" className="flex gap-2 items-center">
-                        Link to rijksmuseum <FiExternalLink size={16} />
-                     </a>
-                  </p>
-               </div>
-               <img src={data.artObjects[0].webImage.url} alt="" />
-            </div>
+            <Hero 
+               artObject={data.artObjects[0]}
+            />
             <div className="grid grid-cols-3 gap-10 py-10">
                {[...data.artObjects].splice(1).map((item, i) => (
                   <div
