88a59b1fa8559987cd32cb02b1714928ee86b70a
LaupWing <loc_strict@hotmail.com>
2023-05-12 18:35:01 +0200

    fock

--- nextjs-12/src/utils/axios.ts
+++ nextjs-12/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-   baseURL: `https://www.rijksmuseum.nl/api/nl/collection?key=FZJhvrzH`,
+   baseURL: `https://www.rijksmuseum.nl/api/nl/collection?key=${process.env.RIJKS_MUSEUM_APIKEY}`,
    timeout: 4000,
 })
 
