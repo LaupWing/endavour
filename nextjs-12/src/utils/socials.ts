682682eed86c369711cf3190c1c159ccf8e1ad71
LaupWing <loc_strict@hotmail.com>
2023-05-12 18:11:19 +0200

    socials

---
+++ nextjs-12/src/utils/socials.ts
@@ -0,0 +1,9 @@
+import { FacebookIcon, InstagramIcon, PinterestIcon, TwitterIcon, YoutubeIcon } from "@/components"
+
+export default {
+   facebook: FacebookIcon,
+   instagram: InstagramIcon,
+   twitter: TwitterIcon,
+   youtube: YoutubeIcon,
+   pinterest: PinterestIcon
+}
