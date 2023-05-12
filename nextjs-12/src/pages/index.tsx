c4c41ef43b7e5f6b206537119f4f69dd44d782e1
LaupWing <loc_strict@hotmail.com>
2023-05-12 11:39:27 +0200

    udpate

--- nextjs-12/src/pages/index.tsx
+++ nextjs-12/src/pages/index.tsx
@@ -1,23 +1,32 @@
 import { GetServerSideProps } from "next"
 import axios from "@/utils/axios"
 import { playfair, poppins } from "@/utils/fonts"
-
+import { BsSearch } from "react-icons/bs"
 
 interface PageProps {
 
 }
 
 export default function Home({data}: any) {
-   // console.log(data)
+   console.log(data)
    return (
       <main
-         className={`flex bg-contrast py-6 px-10 min-h-screen w-screen flex-col justify-between ${poppins.className}`}
+         className={`flex bg-contrast py-6 px-14 min-h-screen w-screen flex-col justify-between ${poppins.className}`}
       >
-         <header className="flex border-b border-primary pb-4">
+         <header className="flex border-b border-primary pb-4 justify-between">
             <h1 className={`${playfair.className} font-bold text-2xl flex flex-col leading-5`}>
                <span className="text-accent">Rijks</span>
                <span className="">Museum</span>
             </h1>
+            <div className="flex items-center mr-4">
+               <nav className="mr-8">
+                  <ul className="flex gap-4">
+                     <li>Works</li>
+                     <li>Artists</li>
+                  </ul>
+               </nav>
+               <BsSearch size={22}/>
+            </div>
          </header>
       </main>
    )
