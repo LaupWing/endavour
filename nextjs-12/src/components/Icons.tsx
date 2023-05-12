6f9cdbe4ad58f9b91033c3e11693863bf79cbf3d
LaupWing <loc_strict@hotmail.com>
2023-05-12 18:08:42 +0200

    update

---
+++ nextjs-12/src/components/Icons.tsx
@@ -0,0 +1,42 @@
+import { IconType } from "react-icons"
+import { AiFillInstagram } from "react-icons/ai"
+
+export const InstagramIcon: IconType = (props) => {
+   return (
+      <AiFillInstagram 
+         {...props}
+      />
+   )
+}
+
+export const InstagramIcon: IconType = (props) => {
+   return (
+      <AiFillInstagram 
+         {...props}
+      />
+   )
+}
+
+export const InstagramIcon: IconType = (props) => {
+   return (
+      <AiFillInstagram 
+         {...props}
+      />
+   )
+}
+
+export const InstagramIcon: IconType = (props) => {
+   return (
+      <AiFillInstagram 
+         {...props}
+      />
+   )
+}
+
+export const InstagramIcon: IconType = (props) => {
+   return (
+      <AiFillInstagram 
+         {...props}
+      />
+   )
+}
