c489e8e3ee62b9c3772b4acacd3b30c97f7363a1
LaupWing <loc_strict@hotmail.com>
2023-05-13 07:13:36 +0200

    added disable link

--- nextjs-12/src/components/ImageContainer.tsx
+++ nextjs-12/src/components/ImageContainer.tsx
@@ -4,10 +4,24 @@ import { FC } from "react"
 export const ImageContainer:FC<{
    url: string
    objectId: string
+   disableLink?: boolean
 }> = ({
    url,
-   objectId
+   objectId,
+   disableLink = false
 }) => {
+   if(disableLink){
+      return (
+         <div className="w-full h-full">
+            <img 
+               className="h-full w-full object-cover" 
+               src={url} 
+               alt="Artwork" 
+            />
+         </div>
+      )
+   }
+
    return (
       <Link 
          className="w-full h-full"
