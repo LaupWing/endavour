8fb79fbae4427a43fc1670a571bb1e743c34216b
LaupWing <loc_strict@hotmail.com>
2023-05-13 07:51:24 +0200

    udpat

--- nextjs-12/src/pages/artwork/[id].tsx
+++ nextjs-12/src/pages/artwork/[id].tsx
@@ -13,9 +13,35 @@ const ArtworkDetail:NextPage<PageProps> = ({ data }) => {
          <Hero 
             artObject={data.artObject}
          />
-         <div className="flex gap-2">
-            <div className="w-8 h-8 rounded-full border-2 border-black"></div>
+         <div className="flex gap-2 items-center">
+            <h3 className="font-bold">Colors</h3>
+            {data.artObject.colors.map((color: any) => (
+               <div 
+                  className="w-8 h-8 rounded-full border-2 border-black"
+                  style={{
+                     backgroundColor: color.hex
+                  }}
+               />
+            ))}
          </div>
+         <table className="table-auto0 rounded overflow-hidden shadow mt-4">
+            <thead className="bg-gray-300">
+               <tr>
+                  <th className="py-2 px-8">Type</th>
+                  <th className="py-2 px-8">Value</th>
+                  <th className="py-2 px-8">Unit</th>
+               </tr>
+            </thead>
+            <tbody>
+               {data.artObject.dimensions.map((size: any) => (
+                  <tr>
+                     <td className="px-2 py-1">{size.type}</td>
+                     <td className="px-2 py-1">{size.value}</td>
+                     <td className="px-2 py-1">{size.unit}</td>
+                  </tr>
+               ))}
+            </tbody>
+         </table>
       </Layout>
    )
 }
