15b12f2e20f8345c601b290f6c114d3da578f4a1
LaupWing <loc_strict@hotmail.com>
2023-05-15 17:25:35 +0200

    added disable link

--- vue3/src/components/ImageContainer.vue
+++ vue3/src/components/ImageContainer.vue
@@ -1,18 +1,41 @@
 <template>
+   <div 
+      className="w-full h-full"
+      v-if="disableLink"
+   >
+      <img 
+         className="h-full w-full object-cover" 
+         :src="url" 
+         alt="Artwork" 
+      />
+   </div>
    <RouterLink
+      v-else
       :to="{
          name: 'Detail',
          params: {
-            id: ''
+            id: objectId
          }
       }"
    >
-      
+      <img 
+         class="h-full w-full object-cover" 
+         :src="url"
+         alt="Artwork" 
+      />
+      <div class="absolute opacity-0 duration-300 group-hover:opacity-100 inset-0 bg-contrast/40 text-accent flex items-center justify-center">
+         <h3 class="text-2xl scale-150 group-hover:scale-100  duration-300 font-display">Read more</h3>
+      </div>
    </RouterLink>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from "vue-router"
+export interface Props {
+   url: string
+   objectId: string
+   disableLink?: boolean
+}
 
-
+defineProps<Props>()
 </script>
