b0050ef944e8e7de3935843c26cbf0901b373ca4
LaupWing <loc_strict@hotmail.com>
2023-05-15 14:46:40 +0200

    art works

--- nextjs-12/src/pages/search.tsx
+++ nextjs-12/src/pages/search.tsx
@@ -10,10 +10,27 @@ interface PageProps {
 const Search:NextPage<PageProps> = ({
    art_works
 }) => {
-   console.log(art_works)
+   const router = useRouter()
+   
    return (
       <Layout>
-         
+         <div className="flex flex-col gap-4">
+            <h2 className="text-xl">Total {art_works.length} results found on search term: {router.query.searchTerm}</h2>
+            {art_works.map(art_work => (
+               <div
+                  key={art_work.id}
+                  className="flex"
+               >
+                  <div className="w-32 aspect-square">
+                     <img className="h-full w-full object-cover" src={art_work.webImage.url} alt="" />
+                  </div>
+                  <div className="ml-2 py-2">
+                     <h2>{art_work.title}</h2>
+                     <p className="text-sm">By <span>{art_work.principalOrFirstMaker}</span></p>
+                  </div>
+               </div>
+            ))}
+         </div>
       </Layout>
    )
 }
