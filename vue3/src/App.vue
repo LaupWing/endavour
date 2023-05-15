d32264439441a891a75e4544167e8b19d8a13fb9
LaupWing <loc_strict@hotmail.com>
2023-05-15 15:54:00 +0200

    udpate

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,15 +1,5 @@
 <template>
-   <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-         <HelloWorld msg="You did it!" />
-
-         <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-         </nav>
-      </div>
+   <header class="w-full bg-red-400 py-4">
    </header>
 
    <RouterView />
