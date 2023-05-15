e11dfe2a7a548bfbb5f4cf03e1724ca89b2162e1
LaupWing <loc_strict@hotmail.com>
2023-05-15 16:10:49 +0200

    udpate

---
+++ vue3/src/components/Layout/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+   <main class="flex bg-contrast min-h-screen fixed inset-0 overflow-y-auto w-screen flex-col">
+      <div class="px-4 md:px-14">
+         <section class="py-10">
+            <slot />
+         </section>
+      </div>
+   </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
