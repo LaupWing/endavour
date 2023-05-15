0a92d17645b0e309b5ccb7b2129d5b8a49c530db
LaupWing <loc_strict@hotmail.com>
2023-05-15 16:42:49 +0200

    update

---
+++ vue3/src/components/Layout/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+   <footer class="w-full border-t border-primary grid grid-cols-1 md:grid-cols-5 mt-auto">
+      <div class="col-span-2 p-4 px-10 md:border-r border-primary flex flex-col">
+         <p class="md:text-2xl text-lg uppercase max-w-[80%] md:max-w-[60%]">Signup to our news letter!</p>
+         <p class="text-sm mt-2 ml-auto text-primary/40 flex items-center">
+            ENTER YOUR EMAIL
+            <ArrowRightIcon class="ml-1 text-accent" size={22} />
+         </p>
+      </div>
+      <div class="col-span-1 grid grid-cols-2 py-4 md:py-0 text-sm border-t md:border-t-0 md:border-r border-primary">
+         <ul class="flex flex-col items-center justify-center">
+            <div>
+               <li>About</li>
+               <li>Blogs</li>
+               <li>Videos</li>
+               <li>Press</li>
+            </div>
+         </ul>
+         <ul class="flex flex-col items-center justify-center">
+            <div>
+               <li>Visit</li>
+               <li>Events</li>
+               <li>Exhibitions</li>
+               <li>Art</li>
+            </div>
+         </ul>
+      </div>
+   </footer>
+</template>
+
+<script setup lang="ts">
+import ArrowRightIcon from "../icons/ArrowRightIcon.vue"
+
+
+</script>
