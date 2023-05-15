32f3f12aeefedcb51ae55c08dcc9c5a07334ae5d
LaupWing <loc_strict@hotmail.com>
2023-05-15 16:19:50 +0200

    update

--- vue3/src/components/Layout/Header.vue
+++ vue3/src/components/Layout/Header.vue
@@ -1,9 +1,66 @@
 <template>
-   <div>
-
-   </div>
+   <header class="flex border-b border-primary py-4 justify-between sticky top-0 bg-contrast z-50">
+      <form 
+         class="absolute inset-0 bg-contrast/95 flex items-center p-2"
+         v-if="showError"
+      >
+         <div className="flex flex-1 items-center relative">
+            <input 
+               type="text"
+               :class="clsx(
+                  'flex-1 rounded'
+               )"
+               v-model="searchInput"
+               placeholder="What are you searching for?"
+            >
+            <span 
+               class="absolute px-2 pt-[2px] bottom-0 translate-y-full bg-red-500 left-1 rounded-b text-contrast text-[8px] uppercase"
+               v-if="showError"
+            >
+               Search field cannot be empty!
+            </span>
+            <button 
+               type="submit"
+               class="absolute right-4 text-gray-400" 
+            >
+               <SearchIcon />
+            </button>
+         </div>
+         <button 
+            className="px-1"
+            @click="showSearch = false"
+            type="button"
+         >
+            <CloseIcon className="text-slate-500" />
+         </button>
+      </form>
+      <RouterLink to="/">
+         <h1 class="font-bold text-2xl flex flex-col leading-5">
+            <span class="text-accent">Rijks</span>
+            <span>Museum</span>
+         </h1>
+      </RouterLink>
+      <div className="flex items-center mr-1 md:mr-4">
+         <nav className="mr-8">
+            <ul className="flex gap-4">
+               <li>Works</li>
+               <li>Artists</li>
+            </ul>
+         </nav>
+         <button @click="showSearch = false">
+            <SearchIcon />
+         </button>
+      </div>
+   </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue"
+import clsx from "clsx"
+import SearchIcon from "../icons/SearchIcon.vue"
+import { RouterLink } from "vue-router"
 
+const showSearch = ref<boolean>(false)
+const showError = ref<boolean>(false)
+const searchInput = ref<string>("")
 </script>
