20c565abae1b64806065efc0b58985a5b1492468
LaupWing <loc_strict@hotmail.com>
2023-05-15 14:50:24 +0200

    added higlither

--- nextjs-12/src/pages/search.tsx
+++ nextjs-12/src/pages/search.tsx
@@ -2,6 +2,7 @@ import { Layout } from "@/components"
 import { GetServerSideProps, NextPage } from "next"
 import { useRouter } from "next/router"
 import axios from "../utils/axios"
+import Highlighter from "react-highlight-words"
 
 interface PageProps {
    art_works: ApiIndexResponse["artObjects"]
@@ -11,11 +12,12 @@ const Search:NextPage<PageProps> = ({
    art_works
 }) => {
    const router = useRouter()
+   const { searchTerm } = router.query
    
    return (
       <Layout>
          <div className="flex flex-col gap-4">
-            <h2 className="text-xl">Total {art_works.length} results found on search term: {router.query.searchTerm}</h2>
+            <h2 className="text-xl">Total {art_works.length} results found on search term: {searchTerm}</h2>
             {art_works.map(art_work => (
                <div
                   key={art_work.id}
@@ -25,8 +27,21 @@ const Search:NextPage<PageProps> = ({
                      <img className="h-full w-full object-cover" src={art_work.webImage.url} alt="" />
                   </div>
                   <div className="ml-2 py-2">
-                     <h2>{art_work.title}</h2>
-                     <p className="text-sm">By <span>{art_work.principalOrFirstMaker}</span></p>
+                     <h2>
+                        <Highlighter 
+                           searchWords={(searchTerm! as string).split(" ")}
+                           textToHighlight={art_work.title}
+                        />
+                     </h2>
+                     <p className="text-sm">
+                        By 
+                        <span>
+                           <Highlighter 
+                              searchWords={(searchTerm! as string).split(" ")}
+                              textToHighlight={art_work.principalOrFirstMaker}
+                           />
+                        </span>
+                     </p>
                   </div>
                </div>
             ))}
