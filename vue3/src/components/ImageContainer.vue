9a761bdab54cefe9fe2724284a34283289cbb275
LaupWing <loc_strict@hotmail.com>
2023-05-15 17:22:32 +0200

    detail

---
+++ vue3/src/components/ImageContainer.vue
@@ -0,0 +1,18 @@
+<template>
+   <RouterLink
+      :to="{
+         name: 'Detail',
+         params: {
+            id: ''
+         }
+      }"
+   >
+      
+   </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router"
+
+
+</script>
