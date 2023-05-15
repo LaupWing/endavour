9a2323c6b25213758b4782eaf6daeb39cc3dbfee
LaupWing <loc_strict@hotmail.com>
2023-05-15 17:31:45 +0200

    added hero section

--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
    <main>
+      <Hero
+         v-if="artWorkStore.artWorks.length"
+         :art-object="artWorkStore.artWorks[0]"
+      />
    </main>
 </template>
 
 <script setup lang="ts">
+import Hero from "@/components/Hero.vue";
 import { useArtWorkStore } from "@/stores/artWorks"
 
 const artWorkStore = useArtWorkStore()
