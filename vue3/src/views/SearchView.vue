5adef20cb4b9c3d6639b32d17b4f389105cd4539
LaupWing <loc_strict@hotmail.com>
2023-05-16 05:58:55 +0200

    udpate

--- vue3/src/views/SearchView.vue
+++ vue3/src/views/SearchView.vue
@@ -5,5 +5,13 @@
 </template>
 
 <script setup lang="ts">
+import { useRoute } from "vue-router"
 
+export interface Props {
+   artWorks: ApiIndexResponse["artObjects"]
+}
+const route = useRoute()
+const { searchTerm } = route.query
+
+defineProps<Props>()
 </script>
