2126d5799f0db89db229c55557f26516ae144aa8
LaupWing <loc_strict@hotmail.com>
2023-05-16 13:40:38 +0200

    update

---
+++ larvel-vue-inertia/resources/js/Components/Grid.vue
@@ -0,0 +1,31 @@
+<template>
+   <div
+      class=" flex flex-col justify-between"
+      :class="index % 3 === 0 ? 'col-span-2 aspect-video' : ''" 
+   >
+      <ImageContainer
+         :url="artObject.webImage.url"
+         :objectId="artObject.objectNumber"
+      />
+      <div class="mt-2 leading-4 flex justify-between items-center">
+         <div class="flex flex-col">
+            <p>{{artObject.title}}</p>
+            <p class="text-xs">By <span class="text-accent">{{artObject.principalOrFirstMaker}}</span></p>
+         </div>
+         <a :href="artObject.links.web" target="_blank" rel="noopener noreferrer">
+            <FiExternalLink class="text-accent cursor-pointer" />
+         </a>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+import ImageContainer from "./ImageContainer.vue"
+
+export interface GridProps {
+   artObject: ArtObjectType
+   index: number
+}
+
+defineProps<GridProps>()
+</script>
--- larvel-vue-inertia/resources/js/Pages/Index/Index.vue
+++ larvel-vue-inertia/resources/js/Pages/Index/Index.vue
@@ -1,14 +1,20 @@
 <template>
-   <div>
-      <Hero
-         :art-object="data[0]"
+   <Hero
+      :art-object="data[0]"
+   />
+   <div class="grid grid-cols-2 md:grid-cols-3 gap-10 py-10">
+      <Grid 
+         v-for="(item, index) in [...data].splice(1)"
+         :key="item.id"
+         :index="index"
+         :art-object="item"
       />
    </div>
 </template>
 
 <script setup lang="ts">
-import { Link } from "@inertiajs/vue3"
 import Hero from "@/Components/Hero.vue"
+import Grid from "@/Components/Grid.vue"
 
 defineProps<{
    data: ApiIndexResponse["artObjects"]
