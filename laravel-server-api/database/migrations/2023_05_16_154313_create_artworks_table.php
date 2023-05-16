b84a934c48f10d26fce1143b5104ce3b8cdfd916
LaupWing <loc_strict@hotmail.com>
2023-05-16 17:57:37 +0200

    udpate

--- laravel-server-api/database/migrations/2023_05_16_154313_create_artworks_table.php
+++ laravel-server-api/database/migrations/2023_05_16_154313_create_artworks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     */
    public function up(): void
    {
-      Schema::create('artworks', function (Blueprint $table) {
+      Schema::create("artworks", function (Blueprint $table) {
          $table->id();
          $table->string("objectNumber")->unique();
          $table->string("title");
@@ -31,6 +31,6 @@ return new class extends Migration
     */
    public function down(): void
    {
-      Schema::dropIfExists('artworks');
+      Schema::dropIfExists("artworks");
    }
 };
--- laravel-server-api/database/migrations/2023_05_16_155231_create_web_images_table.php
+++ laravel-server-api/database/migrations/2023_05_16_155231_create_web_images_table.php
@@ -6,22 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('web_images', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("web_images", function (Blueprint $table) {
+         $table->id();
+         $table->string("guid")->unique();
+         $table->timestamps();
+         $table->foreignIdFor(
+            \App\Models\Artwork::class,
+            "artwork_id"
+         )->constrained("artworks")->onDelete("cascade");
+         $table->integer("offsetPercentageX")->default(0);
+         $table->integer("offsetPercentageY")->default(0);
+         $table->integer("width");
+         $table->integer("height");
+         $table->string("url");
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('web_images');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("web_images");
+   }
 };
