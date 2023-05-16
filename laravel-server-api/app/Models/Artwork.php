79f2e1c8ffe6b985833009b272a3f0ddf5dd619a
LaupWing <loc_strict@hotmail.com>
2023-05-16 18:59:21 +0200

    UPDATE

--- laravel-server-api/app/Models/Artwork.php
+++ laravel-server-api/app/Models/Artwork.php
@@ -18,6 +18,6 @@ class Artwork extends Model
 
    public function productionPlaces()
    {
-      return $this->belongsToMany(ProductionPlace::class);
+      return $this->belongsToMany(ProductionPlace::class, "artwork_production_place");
    }
 }
