b75d2eb003fa04a4534608118be2b23885c8a942
LaupWing <loc_strict@hotmail.com>
2023-05-16 06:01:26 +0200

    udpate

--- vue3/src/views/SearchView.vue
+++ vue3/src/views/SearchView.vue
@@ -1,11 +1,42 @@
 <template>
-   <div>
-      Search View
+   <div className="flex flex-col gap-4">
+      <h2 className="text-xl">Total {{artWorks.length}} results found on search term: {{searchTerm}}</h2>
+      <RouterLink
+         v-for="artWork in artWorks"
+         :to="`/artwork/${artWork.objectNumber}`"
+         :key="artWork.id"
+         className="flex"
+      >
+         <div className="md:w-32 w-20 flex-shrink-0 aspect-square">
+            <img 
+               className="h-full w-full object-cover" 
+               :src="artWork.webImage.url" 
+               alt="result image" 
+            />
+         </div>
+         <div className="ml-2 py-2">
+            <h2 className="md:text-base text-sm">
+               <!-- <Highlighter 
+                  searchWords={(searchTerm! as string).split(" ")}
+                  textToHighlight={artWork.title}
+               /> -->
+            </h2>
+            <p className="md:text-sm text-xs">
+               By 
+               <span>
+                  <!-- <Highlighter 
+                     searchWords={(searchTerm! as string).split(" ")}
+                     textToHighlight={artWork.principalOrFirstMaker}
+                  /> -->
+               </span>
+            </p>
+         </div>
+      </RouterLink>
    </div>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from "vue-router"
+import { RouterLink, useRoute } from "vue-router"
 
 export interface Props {
    artWorks: ApiIndexResponse["artObjects"]
