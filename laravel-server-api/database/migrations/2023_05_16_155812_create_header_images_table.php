d3e8ee5e4f165c9677c8ecb36e20cceb0f21ba76
LaupWing <loc_strict@hotmail.com>
2023-05-16 18:00:39 +0200

    udpate

---
+++ laravel-server-api/database/migrations/2023_05_16_155812_create_header_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create("header_images", function (Blueprint $table) {
+         $table->id();
+         $table->string("guid")->unique();
+         $table->timestamps();
+         $table->foreignIdFor(
+            \App\Models\Artwork::class,
+            "artwork_id"
+         )->constrained("artworks")->onDelete("cascade");
+         $table->integer("offsetPercentageX")->default(0);
+         $table->integer("offsetPercentageY")->default(0);
+         $table->integer("width");
+         $table->integer("height");
+         $table->string("url");
+      });
+   }
+
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists("header_images");
+   }
+};
